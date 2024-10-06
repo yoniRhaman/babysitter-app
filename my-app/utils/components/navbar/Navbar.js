476e83ad564739a86c204d2d6a952dac585abedb
yonatan.r <y.rahman770770@gmail.com>
2024-10-06 21:27:30 +0300

    add navbar

--- my-app/utils/components/navbar/Navbar.js
+++ my-app/utils/components/navbar/Navbar.js
@@ -1,28 +1,71 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
 
-const Navbar = () => {
+const AntTabs = styled(Tabs)({
+  borderBottom: "1px solid #e8e8e8",
+  "& .MuiTabs-indicator": {
+    backgroundColor: "#1890ff",
+  },
+});
+
+const AntTab = styled((props) => <Tab disableRipple {...props} />)(
+  ({ theme }) => ({
+    textTransform: "none",
+    minWidth: 0,
+    [theme.breakpoints.up("sm")]: {
+      minWidth: 0,
+    },
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing(1),
+    color: "rgba(0, 0, 0, 0.85)",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:hover": {
+      color: "#40a9ff",
+      opacity: 1,
+    },
+    "&.Mui-selected": {
+      color: "#1890ff",
+      fontWeight: theme.typography.fontWeightMedium,
+    },
+    "&.Mui-focusVisible": {
+      backgroundColor: "#d1eaff",
+    },
+  })
+);
+
+export default function CustomizedTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '10px', position: 'relative' }}>
-      {/* Logo on the left */}
-      <div style={{ position: 'absolute', left: '10px' }}>
-        <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
-      </div>
-      {/* Centered list of links */}
-      <ul style={{ display: 'flex', listStyle: 'none', justifyContent: 'center', alignItems: 'center', margin: 0, padding: 0 }}>
-        <li style={{ marginRight: '20px' }}>
-          <Link href="/">Home</Link>
-        </li>
-        <li style={{ marginRight: '20px' }}>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ bgcolor: "#fff" }}>
+        <AntTabs value={value} onChange={handleChange} aria-label="ant example">
+          <AntTab label="Tab 1" />
+          <AntTab label="Tab 2" />
+          <AntTab label="Tab 3" />
+          <AntTab label="Tab 4" />
+        </AntTabs>
+        <Box sx={{ p: 4 }} />
+      </Box>
+    </Box>
   );
-};
-
-export default Navbar;
+}
